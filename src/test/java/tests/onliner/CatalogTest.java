10cf833acbb98aec7da10e0e1b9675f726199a3f
denis <denis.dev.mart17@gmail.com>
2020-07-31 09:48:09 +0300

    added Catalog Onliner test

--- src/test/java/tests/onliner/CatalogTest.java
+++ src/test/java/tests/onliner/CatalogTest.java
@@ -31,4 +31,50 @@ public class CatalogTest extends BaseWebTest {
 
         assertThat(actualNavigationItems).isEqualTo(CatalogPage.CatalogNavigationItem.getListOfValues());
     }
+
+    @Test
+    @Tag("all")
+    @Tag("negative")
+    @Feature(value = "Catalog")
+    @Link(value = "Link to Test Case")
+    @DisplayName("Test that the verify specific category navigation item exist. Negative")
+    public void specificNavigationItemExistTest() {
+        OnlinerPage onlinerPage = new OnlinerPage();
+        onlinerPage.navigateTo(OnlinerPage.NavigationItem.CATALOG);
+
+        CatalogPage catalogPage = new CatalogPage();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.ELECTRONICS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.COMPUTERS_AND_NETWORKS)).isFalse();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.HOUSEHOLD_APPLIANCES)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.CONSTRUCTION_AND_REPAIR)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.HOME_AND_GARDEN)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.AUTO_AND_MOTORCYCLE)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.BEAUTY_AND_SPORTS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.FOR_CHILDREN_AND_MOTHERS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.WORK_AND_OFFICE)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.FOOD)).isTrue();
+    }
+
+    @Test
+    @Tag("all")
+    @Tag("negative")
+    @Feature(value = "Catalog")
+    @Link(value = "Link to Test Case")
+    @DisplayName("Test that the verify specific category navigation item exist (Soft Assertion). Negative")
+    public void softAssertionSpecificNavigationItemExistTest() {
+        OnlinerPage onlinerPage = new OnlinerPage();
+        onlinerPage.navigateTo(OnlinerPage.NavigationItem.CATALOG);
+
+        CatalogPage catalogPage = new CatalogPage();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.ELECTRONICS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.COMPUTERS_AND_NETWORKS)).isFalse();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.HOUSEHOLD_APPLIANCES)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.CONSTRUCTION_AND_REPAIR)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.HOME_AND_GARDEN)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.AUTO_AND_MOTORCYCLE)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.BEAUTY_AND_SPORTS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.FOR_CHILDREN_AND_MOTHERS)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.WORK_AND_OFFICE)).isTrue();
+        assertThat(catalogPage.isNavigationItemExist(CatalogPage.CatalogNavigationItem.FOOD)).isTrue();
+    }
 }
--- src/test/java/tests/onliner/TopNavigationTest.java
+++ src/test/java/tests/onliner/TopNavigationTest.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Link;
 import onliner.web.ui.pages.OnlinerPage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -32,14 +31,14 @@ public class TopNavigationTest extends BaseWebTest {
 
     @Test
     @Tag("all")
-    @Tag("navigation")
+    @Tag("negative")
     @Feature(value = "Menu Navigation")
     @Link(value = "Link to Test Case")
     @DisplayName("Test that the verify specific navigation item exist. Negative")
-    public void concreteNavigationItemExistTest() {
+    public void specificNavigationItemExistTest() {
         OnlinerPage onlinerPage = new OnlinerPage();
         assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.CATALOG)).isTrue();
-        assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.NEWS)).isTrue();
+        assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.NEWS)).isFalse();
         assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.AUTO_FLEA_MARKET)).isTrue();
         assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.HOUSES_AND_APARTMENTS)).isTrue();
         assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.SERVICES)).isTrue();
@@ -49,13 +48,12 @@ public class TopNavigationTest extends BaseWebTest {
 
     @Test
     @Tag("all")
-    @Tag("navigation")
+    @Tag("negative")
     @Feature(value = "Menu Navigation")
     @Issue(value = "123456")
-    @Disabled
     @Link(value = "Link to Test Case")
     @DisplayName("Test that the verify concrete navigation item exist (Soft Assertion). Negative")
-    public void softAssertionNavigationItemTest() {
+    public void softAssertionSpecificNavigationItemExistTest() {
         OnlinerPage onlinerPage = new OnlinerPage();
         softly.assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.CATALOG)).isTrue();
         softly.assertThat(onlinerPage.isNavigationItemExist(OnlinerPage.NavigationItem.NEWS)).isFalse();
